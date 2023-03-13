8fb5a5639ec3e61949c7859d27298d5569cc59f3
Волков Артём Д19-191-1 <ArtemVolkov2103@users.noreply.github.com>
2023-03-13 23:27:44 +0400

    first commit

---
+++ PowerCollections/PowerCollections.Tests/StackTests.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace PowerCollections.Tests
+{
+    [TestClass]
+    public class StackTests
+    {
+        [TestMethod]
+        [Description("Проверяю порядок заполнения стека")]
+        /// <summary>
+        /// Проверяю порядок заполнения стека 
+        /// </summary>
+        public void СheckingTheOrderOfFilling()
+        {
+            Stack_new<int> stack = new Stack_new<int>(8);
+            for (int i = 1; i <= 8; i++)
+            {
+                stack.Push(i);
+            }
+            int[] array = new int[] { 8, 7, 6, 5, 4, 3, 2, 1 };
+            CollectionAssert.AreEqual(array, stack.ToArray());
+        }
+
+
+        [TestMethod]
+        /// <summary>
+        /// Проверяю, совпадает ли емкость массива с заданным конструктору параметром 
+        /// </summary>
+        public void StackCapacityTest()
+        {
+            Stack_new<int> stack = new Stack_new<int>(8);
+            Assert.AreEqual(8,stack.Capacity);
+        }
+        /// <summary>
+        /// Должен выдать мой exeption, если на вход задать неверные параметры
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(StackException))]
+        [DataRow(0)]
+        [DataRow(-1)]
+        public void ConstructorExeptionTestWithIncorrectInput(int capacity)
+        {
+            Stack_new<int> stack = new Stack_new<int>(capacity);
+        }
+        /// <summary>
+        /// стек создан пустой, добавляю элемент
+        /// Количество элементов в стеке должно стать = 1
+        /// </summary>
+        [TestMethod]
+        public void PushOneElementAndCountElementsInStack()
+        {
+            Stack_new<int> stack = new Stack_new<int>(8);
+            stack.Push(99);
+            Assert.AreEqual(1, stack.Count);
+        }
+        /// <summary>
+        /// первый элемент должен быть тем же, что только что положил
+        /// </summary>
+        [TestMethod]
+        public void IsPushedRight()
+        {
+            Stack_new<int> stack = new Stack_new<int>(8);
+            stack.Push(99);
+            Assert.AreEqual(99, stack.array[0]);
+        }
+        /// <summary>
+        /// правильно ли берется верхнее значение
+        /// </summary>
+        [TestMethod]
+        public void GetTopElement()
+        {
+            Stack_new<int> stack = new Stack_new<int>(8);
+            stack.Push(99);
+            stack.Push(45);
+            Assert.AreEqual(45, stack.Top());
+        }
+        /// <summary>
+        /// Отлавливаю свой exeption при попытке
+        /// посмотреть верхнее значение -пустого- стека
+        /// </summary>
+        [TestMethod]
+        public void GetTopFromEmptyStackException()
+        {
+            Stack_new<int> stack = new Stack_new<int>(8);
+            var ex = Assert.ThrowsException<StackException>(() => stack.Top());
+            Assert.AreEqual("Top: Обращение к области вне диапазона", ex.Message);
+        }
+        /// <summary>
+        /// Выдает ли _Pop верхнее значение
+        /// </summary>
+        [TestMethod]
+        public void IsPoppedElementRight()
+        {
+            Stack_new<int> stack = new Stack_new<int>(8);
+            stack.Push(99);
+            stack.Push(45);
+            Assert.AreEqual(45, stack.Pop());
+        }
+
+        [TestMethod]
+        public void PopFromEmptyStackException()
+        {
+            Stack_new<int> stack = new Stack_new<int>(8);
+            var ex = Assert.ThrowsException<StackException>(() => stack.Pop());
+            Assert.AreEqual("Pop: Обращение к области вне диапазона", ex.Message);
+        }
+
+        [TestMethod]
+        public void StackMoreThanItIsCalculated()
+        {
+            Stack_new<int> stack = new Stack_new<int>(8);
+            for (int i = 1; i <= 8; i++)
+            {
+                stack.Push(i);
+            }
+            var ex = Assert.ThrowsException<StackException>(() => stack.Push(9));
+            Assert.AreEqual("Push: Заполнение области стека вне диапазона", ex.Message);
+        }
+    }
+}
---
+++ PowerCollections/PowerCollections/Stack.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PowerCollections
+{
+    public class Stack_new<T> : IEnumerable<T>
+    {
+        public int Count { get; set; }
+        public int Capacity { get; set; }
+        public List<T> array;
+        public Stack_new(int capacity){
+           if(capacity<1)
+                throw new StackException("Размер стека не может быть меньше, чем 1");
+           Capacity = capacity;
+           List<T> Array = new List<T>(capacity);
+           array = Array;
+        }
+        public Stack_new()
+        {
+            Capacity = 100;
+            List<T> Array = new List<T>(100);
+            array = Array;
+        }
+        public void Push(T value)
+        {
+            //Console.WriteLine("push at " + Count);
+            if(Count < array.Capacity) {
+                array.Add(value);
+                Count++;
+            }
+            else throw new StackException("Push: Заполнение области стека вне диапазона");
+        }
+         public T Pop()
+        {
+            if(Count <= 0 || Count > array.Capacity)
+                throw new StackException("Pop: Обращение к области вне диапазона");
+            Count--;
+            //Console.WriteLine("Pop at " + Count + " result " + array[Count]);
+            T current = array[Count];
+            //T current = Current;
+            array.RemoveAt(Count);
+            return current;
+        }
+         public T Top()
+        {
+            if (Count <= 0 || Count > array.Capacity)
+                throw new StackException("Top: Обращение к области вне диапазона");
+            //Console.WriteLine("Top at " + (Count-1) + " result " + array[Count-1]);
+            return array[Count - 1];
+        }
+
+        private IEnumerator<T> Enumerator()
+        {
+            for(int i = array.Count-1; i >= 0; i--) yield return array[i];
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            return Enumerator();
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return Enumerator();
+        }
+    }
+    class StackException : Exception
+    {
+        public StackException(string message)
+            : base(message) { }
+    }
+
+}
